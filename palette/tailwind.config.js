1de118335833eb489ebed05cdfa6d5ad2b6265e2
Jason Scholtz <33293669+scozu@users.noreply.github.com>
2025-11-07 15:06:55 -0500

    add tailwind config

---
+++ palette/tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        // Background layers
+        karasu: {
+          'bg-0': '#121212',
+          'bg-1': '#1a1a1a', 
+          'bg-2': '#222222',
+          'bg-3': '#2a2a2a',
+          'bg-4': '#333333',
+          'bg-visual': '#2d3437',
+          'bg-search': '#3c4144',
+          
+          // Foreground tones
+          'fg-0': '#d4c5b9',
+          'fg-1': '#c5b6aa',
+          'fg-2': '#a89984', 
+          'fg-3': '#928374',
+          'fg-dim': '#665c54',
+          
+          // Syntax colors
+          'red': '#c4746e',
+          'green': '#95b572',
+          'yellow': '#c4a657',
+          'blue': '#7c9fa8',
+          'purple': '#a987a8',
+          'aqua': '#85a585',
+          'orange': '#d6936b',
+          
+          // Bright accents
+          'bright-red': '#ea6962',
+          'bright-green': '#a9b665',
+          'bright-yellow': '#d8a657',
+          'bright-blue': '#8ba4b0',
+          'bright-magenta': '#d3869b',
+          'bright-cyan': '#89b482',
+          'bright-white': '#fbf1c7',
+        }
+      }
+    }
+  }
+}
