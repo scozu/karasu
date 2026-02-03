dee3233a68237b8a1aab5d924a4837149104fa1e
Jason Scholtz <33293669+scozu@users.noreply.github.com>
2026-02-03 17:35:44 -0500

    Document karasu install steps

--- lua/karasu/utils.lua
+++ lua/karasu/utils.lua
@@ -6,21 +6,39 @@ local M = {}
 -- Create highlight group
 function M.hl(name, opts)
   opts = opts or {}
-  
+
+  local hl = {}
+  if opts.fg then hl.fg = opts.fg end
+  if opts.bg then hl.bg = opts.bg end
+  if opts.sp then hl.sp = opts.sp end
+  if opts.bold then hl.bold = true end
+  if opts.italic then hl.italic = true end
+  if opts.underline then hl.underline = true end
+  if opts.undercurl then hl.undercurl = true end
+  if opts.strikethrough then hl.strikethrough = true end
+  if opts.reverse then hl.reverse = true end
+  if opts.nocombine then hl.nocombine = true end
+
+  if vim.api and vim.api.nvim_set_hl then
+    vim.api.nvim_set_hl(0, name, hl)
+    return
+  end
+
+  -- Fallback for very old Neovim versions
   local cmd = "highlight " .. name
-  
+
   if opts.fg then
     cmd = cmd .. " guifg=" .. opts.fg
   end
-  
+
   if opts.bg then
     cmd = cmd .. " guibg=" .. opts.bg
   end
-  
+
   if opts.sp then
     cmd = cmd .. " guisp=" .. opts.sp
   end
-  
+
   local attrs = {}
   if opts.bold then table.insert(attrs, "bold") end
   if opts.italic then table.insert(attrs, "italic") end
@@ -29,16 +47,21 @@ function M.hl(name, opts)
   if opts.strikethrough then table.insert(attrs, "strikethrough") end
   if opts.reverse then table.insert(attrs, "reverse") end
   if opts.nocombine then table.insert(attrs, "nocombine") end
-  
+
   if #attrs > 0 then
     cmd = cmd .. " gui=" .. table.concat(attrs, ",")
   end
-  
+
   vim.cmd(cmd)
 end
 
 -- Link highlight groups
 function M.link(from, to)
+  if vim.api and vim.api.nvim_set_hl then
+    vim.api.nvim_set_hl(0, from, { link = to })
+    return
+  end
+
   vim.cmd("highlight link " .. from .. " " .. to)
 end
 
